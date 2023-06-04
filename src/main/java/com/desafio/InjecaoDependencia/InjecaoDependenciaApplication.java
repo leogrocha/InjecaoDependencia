0b859ab6239344989a0fc1defde384e88c04c714
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-06-04 12:02:50 -0300

    Testando os demais casos de testes

--- src/main/java/com/desafio/InjecaoDependencia/InjecaoDependenciaApplication.java
+++ src/main/java/com/desafio/InjecaoDependencia/InjecaoDependenciaApplication.java
@@ -23,9 +23,17 @@ public class InjecaoDependenciaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		DecimalFormat df = new DecimalFormat("#.00");
-		Order order = new Order(13, 150.00, 20.00);
+		Order order = new Order(1034, 150.00, 20.00);
+		Order order2 = new Order(2282, 800.00, 10.00);
+		Order order3 = new Order(1309, 95.90, 0.0);
 		System.out.println("Pedido código: " + order.getOrder());
 		System.out.println("Valor total: R$ " + df.format(service.total(order)));
+		System.out.println("___________________________________________________________");
+		System.out.println("Pedido código: " + order2.getOrder());
+		System.out.println("Valor total: R$ " + df.format(service.total(order2)));
+		System.out.println("___________________________________________________________");
+		System.out.println("Pedido código: " + order3.getOrder());
+		System.out.println("Valor total: R$ " + df.format(service.total(order3)));
 	}
 
 }
