02570740d434d83b4df15c336d175d2fbccba07c
Leonardo Rocha <56287285+leogrocha@users.noreply.github.com>
2023-06-04 11:03:39 -0300

    Initial commit

---
+++ src/test/java/com/desafio/InjecaoDependencia/InjecaoDependenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.InjecaoDependencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjecaoDependenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
